c56b9af3c7f4c766f14dacb1e3f95f4306a22f95
broby1869 <brianmichaelroby@gmail.com>
2023-02-02 21:52:14 -0600

    feat: selection loop created

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ function getPlayerChoice() {
         return playerSelection = null;
     }
 }
+
+let playerWins = 0;
+let computerWins = 0;
+
+function playRound() {
+    for (n = 0;; n++) {
+        getComputerChoice();
+        console.log(computerSelection)
+        getPlayerChoice();
+        if (playerSelection === null) {
+            
+        }
+        else if (playerSelection !== computerSelection && playerSelection !== null) {
+            break;
+        }
+        else {
+            alert(`Player and Computer chose ${computerSelection}, choose again.`)
+        }
+    }
+    console.log(playerSelection, computerSelection);
+}
