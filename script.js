199436ce8438fa960038e35e021b93417f5859d8
broby1869 <brianmichaelroby@gmail.com>
2023-02-02 22:41:58 -0600

    feat:build game and end to game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let computerSelection;
 let playerSelection;
-
+let playerWins = 0;
+let computerWins = 0;
 
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3) + 1;
@@ -27,33 +28,55 @@ function getPlayerChoice() {
     }
 }
 
-let playerWins = 0;
-let computerWins = 0;
-
-for (i = 0; i < 5; i++) {
-    if (playerWins === 3) {
-        alert("The humans live on, for now...")
+function playRound() {
+    for (n = 0;; n++) {
+        getComputerChoice();
+        getPlayerChoice();
+        if (playerSelection === null) {
+    
+        }
+        else if (playerSelection !== computerSelection && playerSelection !== null) {
+            break;
+        }
+        else {
+            alert(`Player and Computer chose ${computerSelection}, choose again.`)
+        }
     }
-    else if (computerWins === 3) {
-        alert("The machines win. Program terminated.")
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        alert("Player wins this round!");
+        playerWins = playerWins + 1;
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        alert("Player wins this round!");
+        playerWins = playerWins + 1;
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        alert("Player wins this round!");
+        playerWins = playerWins + 1;
     }
     else {
-        function playRound() {
-            for (n = 0;; n++) {
-                getComputerChoice();
-                console.log(computerSelection)
-                getPlayerChoice();
-                if (playerSelection === null) {
-            
-                }
-                else if (playerSelection !== computerSelection && playerSelection !== null) {
-                    break;
-                }
-                else {
-                    alert(`Player and Computer chose ${computerSelection}, choose again.`)
-                }
-            }
-            console.log(playerSelection, computerSelection);
+        alert("Computer wins this round...");
+        computerWins = computerWins + 1;
+    }
+}
+
+function game() {
+    for (i = 0; i < 6; i++) {
+        if (playerWins === 3) {
+            alert("The humans live on, for now...")
+            playerWins = 0;
+            computerWins = 0;
+            break;
+        }
+        else if (computerWins === 3) {
+            alert("The machines win. Program terminated.")
+            playerWins = 0;
+            computerWins = 0;
+            break;
+        }
+        else {
+            playRound();
+            alert(`Humans: ${playerWins}, Machines: ${computerWins}`);
         }
     }
 }
