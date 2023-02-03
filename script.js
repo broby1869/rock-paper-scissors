1e566c8d7b5ec74a97e945cb0223264487773cc5
broby1869 <brianmichaelroby@gmail.com>
2023-02-02 20:53:32 -0600

    fix: variable names to selection

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let computerChoice;
-let playerChoice;
+let computerSelection;
+let playerSelection;
 
 
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3) + 1;
     if (x === 1) {
-        return computerChoice = "rock";
+        return computerSelection = "rock";
     }
     else if (x === 2) {
-        return computerChoice = "paper";
+        return computerSelection = "paper";
     }
     else {
-        return computerChoice = "scissors";
+        return computerSelection = "scissors";
     }
 }
 
